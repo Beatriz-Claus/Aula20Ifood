823a592d94e6ce753da3bb803f8caccac5433141
Beatriz-Claus <61596748+Beatriz-Claus@users.noreply.github.com>
2020-06-18 15:30:41 -0300

    Aplicação de Serviço de entregas (Ifood) - Finalizado

---
+++ Cliente.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Aula20Ifood
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string resposta;
+
+        public void MostrarDados(){
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine($"\nConfirme seus dados: \nNome: {Nome} \nEndereço de Entrega: {Endereco} \nResponda com Sim ou Não.");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula20Ifood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            Restaurante info = new Restaurante();
+            info.Nome = "Cantinho da Vovó";
+            info.Endereco = "Rua Almerindo Alziro Paganini, 118, 03671-000 São Paulo";
+            info.MostrarDados();
+
+            //
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Jacira";
+            cliente.Endereco = "Jd. Antartica";
+
+            string resposta = null;
+
+            while(resposta != "Sim"){
+            cliente.MostrarDados();
+            resposta = Console.ReadLine();
+            }
+
+            //
+            Console.ForegroundColor = ConsoleColor.Red;
+            Pedido Lasanha = new Pedido("Lasanha", "Sabores: Calabresa/Queijo/Bolonhesa", "Porção Individual: R$15" );
+            System.Console.WriteLine($"\n{Lasanha.Nome}\n{Lasanha.Descricao}\n{Lasanha.Preco}");
+            Console.ResetColor();
+        }
+    }
+}
